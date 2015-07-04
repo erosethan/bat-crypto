255d18cc35bde4e4642c4a298240d049cdc5f2bb
Ethan Jimenez <erosethanjv@gmail.com>
2015-07-03 19:40:45 -0500

    Contact server V1.0

--- contact_server.py
+++ contact_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import time
 import socket
 import threading
 from rsa import *
@@ -8,6 +9,8 @@ from rsa import *
 class ContactServer:
 
   serv_sock = None
+  is_active = False
+  directory = dict()
 
   def __init__(self, addr, port = 7878):
     self.serv_sock = socket.socket(
@@ -16,23 +19,59 @@ class ContactServer:
     self.serv_sock.bind((addr, port))
     print 'Binding at port %d' % port
 
+  def ReadLine(self, sock):
+    line_buffer = ''
+    while True:
+      try:
+        byte = sock.recv(1)
+        if not byte: break
+        if byte == '\n': break
+        line_buffer += byte
+      except: pass
+    return line_buffer
+
   def Connection(self, sock, addr):
-    print 'Testing'
+    ip = addr[0]
+    sock.settimeout(1)
+    print 'Getting %s\'s key' % ip
+    pkey = self.ReadLine(sock)
+    self.directory[ip] = pkey + '\n'
+    print '%s is now online!' % ip
+
+    while self.is_active:
+      try:
+        cmd = sock.recv(1)
+        if not cmd: break
+        if cmd == 'U':
+          pkey = self.ReadLine(sock)
+          self.directory[ip] = pkey + '\n'
+          print '%s updated his key' % ip
+        elif cmd == 'G':
+          query = self.ReadLine(sock)
+          sock.send(self.directory.get(query, '\n'))
+          print '%s got %s\'s key' % (ip, query)
+      except: pass
+
+    print '%s is now offline!' % ip
+    del self.directory[ip]
+    sock.close()
 
   def Run(self, max_conn = 10):
     try:
+      self.is_active = True
       print 'Starting service now!'
       self.serv_sock.listen(max_conn)
       while True:
         print 'Waiting for new connection...'
         (sock, addr) = self.serv_sock.accept()
-        print 'Connection from %s' % addr[0]
         thread = threading.Thread(
           target = self.Connection,
           args = (sock, addr))
         thread.start()
+        time.sleep(0.1)
     except:
       print '\nServer shutted down'
+      self.is_active = False
       self.serv_sock.close()
 
 if __name__ == '__main__':
